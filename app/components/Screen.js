98d384f956fbd5fd494036cd58f597ec6137fa1d
RANA-USAMA <ranausamauet3430@gmail.com>
2021-10-15 01:14:55 +0500

    First Commit

---
+++ app/components/Screen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Platform, SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import Colors from "../config/Colors";
+
+function Screen({ children, statusBarColor = Colors.primary, style }) {
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      {Platform.OS === "android" ? <StatusBar backgroundColor={statusBarColor} barStyle="dark-content" /> : null}
+      {children}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export default Screen;
---
+++ app/config/Colors.js
@@ -0,0 +1,5 @@
+const Colors = {
+    white: "#fff",
+    black: '#000'
+}
+export default Colors;
